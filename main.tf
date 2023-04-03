01285d48d1f43ff7a89893b8bf8ccb30325c5595
weskhel <weskhel@yahoo.com.br>
2023-04-03 16:55:05 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name_prefix = "web-"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo apt-get update",
+    "sudo apt-get install -y apache2",
+    "sudo systemctl start apache2"
+  ]
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.web.public_ip
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+  }
+}
